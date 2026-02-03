fc3cecea66b8dcb3d9a8b93f19497791757f5ad0
FxaverD <siskus1602@gmail.com>
2026-02-03 09:45:02 +0700

    name,age,status

---
+++ day 3 (day 2 materi)/bio.js
@@ -0,0 +1,5 @@
+import PromptSync from "promptsync"
+var prompt = PromptSync
+var Name = prompt('What is your Name?')
+var age = prompt('How old are you?')
+var status = prompt ('What is your status?')

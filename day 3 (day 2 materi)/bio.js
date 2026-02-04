0c45ff5d450be8e096d2cf0fce3cddc3489b55d3
FxaverD <siskus1602@gmail.com>
2026-02-04 10:56:38 +0700

    gjelas

--- day 3 (day 2 materi)/bio.js
+++ day 3 (day 2 materi)/bio.js
@@ -1,5 +1,13 @@
-import PromptSync from "promptsync"
-var prompt = PromptSync
+import PromptSync from "prompt-sync"
+var prompt = PromptSync()
 var Name = prompt('What is your Name?')
 var age = prompt('How old are you?')
 var status = prompt ('What is your status?')
+
+var birthdate = prompt("Enter your birthdate")
+
+if (age >= 65) {
+    console.log("RETIRED")
+} else {
+    console.log("NOT RETIRED")
+}
